92e24253dcc0c086936cdafcf79e14b28ec2940c
RicardoVolk <junior.volk@gmail.com>
2024-05-31 16:22:43 -0300

    creat multi text bottun widget

--- financy_app/lib/features/onboarding/onboarding_page.dart
+++ financy_app/lib/features/onboarding/onboarding_page.dart
@@ -1,7 +1,11 @@
+import 'dart:developer';
+
 import 'package:financy_app/common/constats/app_colors.dart';
 import 'package:financy_app/common/constats/app_text_styles.dart';
 import 'package:flutter/material.dart';
+import '../../common/widgets/multi_text_button_widget.dart';
 import '../../common/widgets/primary_button.dart';
+
 class OnboardingPage extends StatelessWidget {
   const OnboardingPage({super.key});
 
@@ -19,34 +23,43 @@ class OnboardingPage extends StatelessWidget {
                 child: Image.asset('assets/images/man.png'),
               ),
             ),
-            Text('Spend Smarter', 
-            style: AppTextStyles.mediumText.copyWith(
-              color: AppColors.greelightTwo
-              ),
+            Text(
+              'Spend Smarter',
+              style: AppTextStyles.mediumText
+                  .copyWith(color: AppColors.greelightTwo),
             ),
-            Text('Save More',
-            style: AppTextStyles.mediumText.copyWith(
-              color: AppColors.greelightTwo
-              ),
+            Text(
+              'Save More',
+              style: AppTextStyles.mediumText
+                  .copyWith(color: AppColors.greelightTwo),
             ),
-            
             Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 32.0,
-                vertical: 16.0,
-                ),
+              padding: const EdgeInsets.only(
+                left: 32.0,
+                right: 32.0,
+                top: 16.0,
+                bottom: 4.0,
+              ),
               child: PrimaryButton(
-                onPressed: (){
-                 
-                },
+                onPressed: () {},
                 text: 'Get Started',
               ),
             ),
             const SizedBox(height: 16.0),
-            Text('Alredy have account? Log in',
-            style: AppTextStyles.smallText.copyWith(
-              color: AppColors.gray
-              ),
+            MultiTextButton(
+              onPressed: () => log('tap'),
+              children: [
+                Text(
+                  'Alredy have account? ',
+                  style:
+                      AppTextStyles.smallText.copyWith(color: AppColors.gray),
+                ),
+                Text(
+                  'Log in',
+                  style: AppTextStyles.smallText
+                      .copyWith(color: AppColors.greelightTwo),
+                ),
+              ],
             ),
             const SizedBox(height: 40.0),
           ],
